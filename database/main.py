1c19099c8479cc7d22928c152473c0bd5273bc8e
Dorna Raj Gyawali <dronarajgyawali@gmail.com>
2024-10-30 21:34:20 +0545

    Update main.py

--- database/main.py
+++ database/main.py
@@ -6,31 +6,32 @@ def main():
 
 
     while True:
-        print("\n Welcome to our note app")
-        print("1. Add Content")
-        print("2. View all Content")
-        print("3. View by date")
-        print("4. Delete Content")
-        print("5. Save content")
-        print("6. Exit")
-        choice = input("Choose an option: ")
+    print("\n Welcome to our note app")
+    print("1. Add Content")
+    print("2. View all Content")
+    print("3. View by date")
+    print("4. Delete Content")
+    print("5. Save content")
+    print("6. Exit")
+    choice = input("Choose an option: ")
 
-        if choice=='1':
-            run.create_note()
-        elif choice=='2':
-            run.view_content()
-        elif choice=='3':
-            run.search_date()
-        elif choice=='4':
-            run.delete_content()
-        elif choice=='5':
+    actions = {
+        '1': run.create_note,
+        '2': run.view_content,
+        '3': run.search_date,
+        '4': run.delete_content,
+        '5': run.save_local,
+        '6': lambda: print("You chose to Exit")
+    }
 
-            run.save_local()
-        elif choice=='6':
-            print('You choose to Exit')
-            break
-        else:
-            print('Invaild option')
+    action = actions.get(choice, lambda: print("Invalid option"))
+    
+    # If choice is '6', break the loop after printing the exit message
+    if choice == '6':
+        action()
+        break
+    else:
+        action()
 
 if __name__=='__main__':
     main()
